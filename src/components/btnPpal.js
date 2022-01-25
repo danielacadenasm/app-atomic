b3fc77ea02bb86ab2282f995e64d6e8ee950c540
danielacadenasm <cadenasdaniela26@gmail.com>
2022-01-24 21:41:39 -0600

    distribución del proyecto.

---
+++ src/components/btnPpal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+
+const btnPpal = () => {
+    return (
+        <Button
+          title="¡Quiero ser parte!"
+          titleStyle={{ fontWeight: 'bold', fontSize: 18, color: 'rgba(90, 154, 230, 1)' }}
+        >
+        </Button>
+    );
+};
+
+export default btnPpal;
